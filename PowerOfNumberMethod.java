1037edaf117c92d84611a069e7198abe3face305
SeyfettinKaratas <77740908+SeyfettinKaratas@users.noreply.github.com>
2021-07-29 16:52:34 +0300

    Add files via upload

---
+++ PowerOfNumberMethod.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class PowerOfNumberMethod {
+    public static void main(String[] args) {
+        power();
+    }
+    static void power() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Taban değeri giriniz :");
+        int base = scan.nextInt();
+        System.out.print("Üs değeri giriniz :");
+        int exponent = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+}
